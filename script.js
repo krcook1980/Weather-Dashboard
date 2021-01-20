251a31f31fc00563bfe2f2043b89648f98881b91
Kelly Cook <kellyrcook1980@gmail.com>
2021-01-19 23:42:33 -0700

    working through forecast section, got links working, need to work through formating and icons

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ var cityArray = [];
 var APIKey = "04bb749b9ebcf8398aa868406de732cf";
 var today = moment().format("dddd, MMMM Do YYYY");
 
+
+//upon open of page, get last city search from local storage and display
 cityName = localStorage.getItem("City");
 currInfo();
-foreInfo();
 
-//on click of search button
+
+//on click of search button get weather data and create button
 $(".searchBtn").click(function () {
-
     cityName = $(".searchCity").val();
     cityArray.push(cityName);
     localStorage.setItem("City", [cityName]);
@@ -18,18 +19,19 @@ $(".searchBtn").click(function () {
     renderButtons();
 })
 
-//previous search city buttons, recall info again
-$(".previous").click(function(){
-    var btnCity = $(this).attr("data-city");
-    cityName = btnCity.val();
+//previous search city buttons, recall info again ***NOT WORKING
+$(".previous").click(function () {
+    alert("I was clicked");
+
+    var btnCity = $(this).attr("data-city"); //should return city pushed by render button
+    cityName = btnCity;
     cityArray.push(cityName);
     localStorage.setItem("City", [cityName]);
     currInfo();
-    renderButtons();
-
+   
 })
 
-//get and display current weather info
+//get and display current weather info, must use to get lat and lon for the uv and forecast
 function currInfo() {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=Imperial&appid=" + APIKey;
     
@@ -38,35 +40,35 @@ function currInfo() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        //console.log(response);
+        //console.log("currInfo " + response);
         var fore = response.weather[0].main
             var icon = $("#icon")
             $(".city").text(cityName);
             $(".date").text(today);
             $(".temp").text("Temperature: " + response.main.temp);
             $(".feels").text("Feels Like: " + response.main.feels_like);
-            $(".hum").text("Humidity: " + response.main.humidity);
+            $(".hum").text("Humidity: " + response.main.humidity + "%");
             $(".wind").text("Wind Speed: " + response.wind.speed);
-                if (fore === "rain"){
-                icon.addClass("rain");
-                }
-                 else if (fore === "clouds"){
-                icon.addClass("clouds");
-                }
-                else if (fore === "snow"){
-                icon.addClass("snow");
-                }
-                else{
-                icon.addClass("clear");
-                }
+            //     if (fore === "rain"){
+            //     icon.addClass("rain");
+            //     }
+            //      else if (fore === "clouds"){
+            //     icon.addClass("clouds");
+            //     }
+            //     else if (fore === "snow"){
+            //     icon.addClass("snow");
+            //     }
+            //     else{
+            //     icon.addClass("clear");
+            //     }
             
-            $(".fore").text(fore).append(icon);       
-        //get, post and add color for uv rating 
+            // $(".fore").text(fore).append(icon);       
+        //get, post and add color for uv rating and forecast
             function uvInfo() {
             var lat = response.coord.lat;
             var lon = response.coord.lon;
             
-            var uvURL = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+            var uvURL = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=Imperial&appid=" + APIKey;
             
 
             $.ajax({
@@ -75,6 +77,7 @@ function currInfo() {
             }).then(function (response2) {
                 console.log(response2)
                 var rating = response2.current.uvi
+                //UV rating and color
                 $(".uv").text("UV Rating: " + response2.current.uvi)
                 if (rating <= 2) {
                     $(".uv").addClass("text-success");
@@ -85,26 +88,21 @@ function currInfo() {
                 else if (rating >= 6) {
                     $(".uv").addClass("text-danger");
                 }
+
+                //5 day forecast
+//generate card rather than in html so we can use a for loop here but can we get the days added in there for the date moment?
+
+                $(".date1").text(moment().add(1,'days').format("MMMM Do YYYY"));
+                $(".temp1").text("Temp: " + response2.daily[0].temp.day);
+                $(".hum1").text("Hum: " + response2.daily[0].humidity + "%");
+                $(".icon1").text(response2.daily[0].weather[0].main);
             })
         }
         uvInfo();
     })
 }
 
-//get and display 5 day forecast info
-function foreInfo() {
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=Imperial&appid=" + APIKey;
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        //console.log(response);
-        var dailyArray = [response.daily];
-        console.log(dailyArray);
-       
-    })
-}
+
 
 //add previously searched cities to buttons for reuse
 function renderButtons() {
@@ -119,15 +117,3 @@ function renderButtons() {
     $("#prevSearch").append(buttonDiv);
     $(".searchCity").val("");
 }
-
-
-
-
-//city name date and weather icon, temperature, humidity, wind speed, and uv
-    //uv index color indicates whether favorable moderate or severe
-//below show 5 day forecast with date icon of weather temp, humidity 
-
-//clicking search history displays current and 5 day for that city again
-
-//upon open, show last city searched as beginning point
-
