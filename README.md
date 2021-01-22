dd7adab7a74857585ba4c1aab4fbf249b0c3ebee
Kelly Cook <kellyrcook1980@gmail.com>
2021-01-21 19:11:31 -0700

    functions and format all complete, finishing read me file

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Weather-Dashboard
-https://coolors.co/
-activites 5, 8, 9
+As a traveler, I would like a [weather dashboard](https://krcook1980.github.io/Weather-Dashboard/) to show me the weather for multiple cities.
+- Able to search for a city by name.
+- Search results include city name, date, weather icon, temperature, humidity, and uv index.
+-UV index will notify by color if conditions are favorable, moderate, or severe.
+-5 day forecast with date, temperature, weather icon, and humidity will also be displayed.
+-Cities I have searched will appear in a search history allowing me to select the city again to view the information.
+-When I return to the page, my last searched city information will appear.
+
+## Functionality
+Utilized HTML, Bootstrap, CSS, and jQuery to create a dynamic webpage that is easy to use. Used ajax to reach the api [OpenWeatherMap.org]("https://openweathermap.org/api") in order to populate the weather information. 
+
+Weather icon as well as background image change based on weather type
+
+## Resources
+- [Moment.js](https://momentjs.com/)
+- [Bootstrap](https://getbootstrap.com/)
+- [W3 Schools](https://www.w3schools.com/)
+- [Stackoverflow](https://stackoverflow.com/)
+- [freeformatter](https://www.freeformatter.com/html-validator.html)
+- [coolors.co](https://coolors.co/) for color selection
+- [dovora.com](https://www.dovora.com/resources/weather-icons/) for weather icons
+- Bootcamp Activities 5, 8, 9
--- script.js
+++ script.js
@@ -8,68 +8,82 @@ var today = moment().format("MM/DD/YYYY");
 cityName = localStorage.getItem("City");
 currInfo();
 
-
 //on click of search button get weather data and create button
-$(".searchBtn").click(function () {
+function searchNow (){
     cityName = $(".searchCity").val();
     cityArray.push(cityName);
     localStorage.setItem("City", [cityName]);
     currInfo();
     renderButtons();
-})
+};
 
+$(".searchBtn").click(searchNow);
+
+
+$(".searchCity").keypress(function(e){
+    if(e.which == 13) {
+        searchNow();
+    }
+})
 
 //get and display current weather info, must use to get lat and lon for the uv and forecast
 function currInfo() {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=Imperial&appid=" + APIKey;
 
-
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        var fore = response.weather[0].main
+        var fore = response.weather[0].main;
         var icon = $(".icon");
+        var imageUrl = "";
         console.log(response);
         $(".city").text(response.name, today);
         $(".date").append(" " + today);
         $(".temp").text("Temperature: " + response.main.temp + " °F");
-        $(".feels").text("Feels Like: " + response.main.feels_like);
+        $(".feels").text("Feels Like: " + response.main.feels_like + " °F");
         $(".hum").text("Humidity: " + response.main.humidity + "%");
-        $(".wind").text("Wind Speed: " + response.wind.speed);
-        if (fore === "Rain") {
+        $(".wind").text("Wind Speed: " + response.wind.speed + " mph");
+        if (fore === "Rain" || fore === "Drizzle") {
             icon.attr("src", "./assets/iconRain.png");
-            var imageUrl = "./assets/rain.jpg"
+            imageUrl = "./assets/rain.jpg";
+            $("body").css("background-image", "url(" + imageUrl + ")");
+        }
+        else if (fore === "Thunderstorm") {
+            icon.attr("src", "./assets/iconThunder.png");
+            imageUrl = "./assets/thundert.jpg";
             $("body").css("background-image", "url(" + imageUrl + ")");
         }
         else if (fore === "Clouds") {
             icon.attr("src", "./assets/iconCloud.png");
-            var imageUrl = "./assets/clouds.jpg"
+            imageUrl = "./assets/clouds.jpg";
             $("body").css("background-image", "url(" + imageUrl + ")");
         }
         else if (fore === "Snow") {
             icon.attr("src", "./assets/iconSnow.png");
-            var imageUrl = "./assets/snow.jpg"
+            imageUrl = "./assets/snow.jpg";
             $("body").css("background-image", "url(" + imageUrl + ")");
         }
         else if (fore === "Mist") {
             icon.attr("src", "./assets/iconMist.png");
-            var imageUrl = "./assets/mist.jpg"
+            imageUrl = "./assets/mist.jpg";
             $("body").css("background-image", "url(" + imageUrl + ")");
         }
-        else if (fore === "Fog") {
-            icon.attr("src", "./assets/iconFog.png");
-            var imageUrl = "./assets/fog.jpg"
+        else if (fore === "Clear") {
+            icon.attr("src", "./assets/iconClear.png");
+            imageUrl = "./assets/clear.jpg";
             $("body").css("background-image", "url(" + imageUrl + ")");
         }
         else {
-            icon.attr("src", "./assets/iconClear.png");
-            var imageUrl = "./assets/clear.jpg"
+            icon.attr("src", "./assets/iconFog.png");
+            imageUrl = "./assets/fog.jpg";
             $("body").css("background-image", "url(" + imageUrl + ")");
         }
+        
 
-        $(".fore").text(response.weather[0].description)
-        //get, post and add color for uv rating and forecast
+        $(".fore").text(response.weather[0].description);
+
+        //get, post and add color for uv rating
         function uvInfo() {
             var lat = response.coord.lat;
             var lon = response.coord.lon;
@@ -81,10 +95,10 @@ function currInfo() {
                 url: uvURL,
                 method: "GET"
             }).then(function (response2) {
-                //console.log(response2)
-                var rating = response2.current.uvi
+
+                var rating = response2.current.uvi;
                 //UV rating and color
-                $(".uv").text("UV Rating: " + response2.current.uvi)
+                $(".uv").text("UV Rating: " + response2.current.uvi);
                 if (rating <= 2) {
                     $(".uv").addClass("text-success");
                 }
@@ -95,12 +109,11 @@ function currInfo() {
                     $(".uv").addClass("text-danger");
                 }
 
-                //5 day forecast
-                
-               var daily = response2.daily;
+                //5 day forecast card generation
+                var daily = response2.daily;
                 $(".card-deck").empty();
-                for (var i = 1; i < daily.length-2; i++) {
-                    
+                for (var i = 1; i < daily.length - 2; i++) {
+
                     var card = $("<div></div>");
                     var date = $("<h5 class='card-title'>" + moment(response2.daily[i].dt, "X").format("MM/DD/YYYY") + "</h5>");
                     var temp = $("<p class='card-text'> Temp: " + response2.daily[i].temp.day + " °F </p>");
@@ -108,27 +121,24 @@ function currInfo() {
                     var main = $("<p class='card-text'>" + response2.daily[i].weather[0].main + "</p>");
                     var foreMain = response2.daily[i].weather[0].main;
                     var icon = "";
-                    console.log("test " +response2.daily[i].weather[0].main )
+
                     if (foreMain === "Rain") {
-                       icon = "<img src='./assets/iconRain.png' style='width: 30px;'>";
+                        icon = "<img src='./assets/iconRain.png' style='width: 30px;'>";
                     }
                     else if (foreMain === "Clouds") {
-                       icon = "<img src='./assets/iconCloud.png' style='width: 30px;'>";
-                       console.log("I should " + icon)
+                        icon = "<img src='./assets/iconCloud.png' style='width: 30px;'>";
                     }
                     else if (foreMain === "Snow") {
                         icon = "<img src='./assets/iconSnow.png' style='width: 30px;'>";
-                        console.log("I should " + icon)
-                     }
+                    }
                     else if (foreMain === "Fog") {
-                       icon = "<img src='./assets/iconFog.png' style='width: 30px;'>";
+                        icon = "<img src='./assets/iconFog.png' style='width: 30px;'>";
                     }
                     else if (foreMain === "Mist") {
                         icon = "<img src='./assets/iconMist.png' style='width: 30px;'>";
                     }
                     else {
-                       icon = "<img src='./assets/iconClear.png' style='width: 30px;'>";
-                        
+                        icon = "<img src='./assets/iconClear.png' style='width: 30px;'>";
                     }
 
                     card.addClass("card");
@@ -136,26 +146,24 @@ function currInfo() {
                     $(".card-deck").append(card);
 
                 }
-            })
+            });
 
         }
-        uvInfo()
-    })
+        uvInfo();
+    });
 
 }
-
-
-
 
 //add previously searched cities to buttons for reuse
 function renderButtons() {
     var newCity = cityArray[cityArray.length - 1];
-    var button = $("<button>");
-    var buttonDiv = $("<div>")
+    var button = $("<a>");
+    var buttonDiv = $("<div class='row border search'>");
 
     button.text(newCity);
-    button.addClass("previous");
-    button.attr("data-city", newCity)
+    button.addClass("previous btn btn-block");
+    button.attr("role", "button");
+    button.attr("data-city", newCity);
     buttonDiv.append(button);
     $("#prevSearch").prepend(buttonDiv);
     $(".searchCity").val("");
@@ -167,7 +175,7 @@ function renderButtons() {
         localStorage.setItem("City", [cityName]);
         currInfo();
 
-    })
+    });
 }
 
 
